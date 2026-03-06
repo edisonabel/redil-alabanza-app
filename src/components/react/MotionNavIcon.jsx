cc31479a4d8a40c9c498124d408d405355102cbb
edisonabel <edicionaular@gmail.com>
2026-03-06 17:15:57 -0500

    animacion menu
    MENU

--- src/components/react/MotionNavIcon.jsx
+++ src/components/react/MotionNavIcon.jsx
@@ -5,14 +5,20 @@ export default function MotionNavIcon({ icon = '', active = false }) {
   const prefersReducedMotion = useReducedMotion();
 
   const springTransition = prefersReducedMotion
-    ? { duration: 0 }
-    : { type: 'spring', stiffness: 520, damping: 30, mass: 0.34 };
+    ? { duration: 0.01 }
+    : { type: 'spring', stiffness: 440, damping: 30, mass: 0.42 };
 
   return (
     <motion.div
       data-nav-icon
-      className={`w-6 h-6 mb-1 transition-transform duration-300 ${active ? 'scale-110' : 'scale-100'}`}
-      whileTap={prefersReducedMotion ? undefined : { scale: 0.85 }}
+      className="w-6 h-6 mb-1 origin-center will-change-transform"
+      initial={false}
+      animate={
+        prefersReducedMotion
+          ? { scale: active ? 1.08 : 1, y: 0 }
+          : { scale: active ? 1.13 : 1, y: active ? -1 : 0 }
+      }
+      whileTap={prefersReducedMotion ? undefined : { scale: 0.8, y: 1 }}
       transition={springTransition}
       dangerouslySetInnerHTML={{ __html: icon }}
     />
