7dd0378c4a762e36c0a825193743e51b19da91cb
edisonabel <edicionaular@gmail.com>
2026-08-16 15:55:43 -0500

    Accept database UUIDs in compact setlist IDs

--- src/utils/customSetlistShare.ts
+++ src/utils/customSetlistShare.ts
@@ -1,4 +1,4 @@
-const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 const TOKEN_RE = /^[A-Za-z0-9_-]{22}$/;
 
 const bytesToBase64 = (bytes: Uint8Array) => {
